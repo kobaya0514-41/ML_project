5c1757f71da3557f654493108e5fdf6abb65a088
kobaya0514-41 <36195620+kobaya0514-41@users.noreply.github.com>
2021-09-26 22:48:27 +0900

    Create create_stratifiedfold.py

---
+++ src/create_stratifiedfold.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from sklearn import model_selection
+
+if __name__ == "__main__":
+    df = pd.read_csv("project/input/adult.csv")
+
+    df["kfold"] = -1
+
+    df = df.sample(frac = 1).reset_index(drop = True)
+
+    y = df.target.values
+
+    kf = model_selection.StratifiedKFold(n_splits = 5)
+
+    for fold,(trn_,val_) in enumerate(kf.split(X = df ,y = y)):
+        df.loc[val_,'kfold'] = fold
+
+        df.to_csv("project/input/adult_folds.csv" , index = False)
