b4cc5925f281a8596c4106a4589babfd95c1cf18
kobaya0514-41 <36195620+kobaya0514-41@users.noreply.github.com>
2021-09-26 23:09:51 +0900

    Update create_stratifiedfold.py

--- src/create_stratifiedfold.py
+++ src/create_stratifiedfold.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sklearn import model_selection
 
 if __name__ == "__main__":
-    df = pd.read_csv("project/input/adult.csv")
+    df = pd.read_csv("project/input/filename.csv")
 
     df["kfold"] = -1
 
